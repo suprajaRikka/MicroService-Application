a05a034e80de7c9e797faf99e5842efac3a77472
supraja <suprajarikka@gmail.com>
2025-03-12 23:34:25 -0500

    "added the files to staged area"

---
+++ QuizApp/src/main/java/com/microservice/QuizApp/QuestionDao.java
@@ -0,0 +1,24 @@
+package com.microservice.QuizApp;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.criteria.Order;
+
+@Repository
+
+public interface QuestionDao extends JpaRepository<Question, Integer>  {
+	
+	List<Question> findByCategory(String category);
+	
+//	@Query(value =" SELECT * FROM question q where q.category=:category" ORDER BY RANDOM() LIMIT :numQ, nativeQuery = true)
+//	
+//	List<Question> findRandomQuestionsByCategory(String category, int numQ);
+
+	@Query(value = "SELECT * FROM question q WHERE q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+	List<Question> findRandomQuestionsByCategory(String category, int numQ);
+
+}
---
+++ QuizApp/src/test/java/com/microservice/QuizApp/QuestionTest.java
@@ -0,0 +1,14 @@
+package com.microservice.QuizApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuestionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
